d3348747903ac68caab28779313dc1da5bd473ce
Walace Rodrigues <walace.maia@totvs.com.br>
2020-05-16 19:15:03 -0300

    deploy

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-
-//Install express server
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo-angular-po-ui'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/demo-angular-po-ui/index.html'));
+ 
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+ 
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
-
-// Start the app by listening on the default Heroku port
+ 
 app.listen(process.env.PORT || 8080);
+
+
+
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/demo-angular-po-ui'));
+
+// app.get('/*', function(req,res) {
+    
+// res.sendFile(path.join(__dirname+'/dist/demo-angular-po-ui/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
